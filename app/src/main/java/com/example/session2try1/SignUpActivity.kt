80f57b0bba3d30ba31e2844df0b2ae44074118a3
asd <zolotarev_d_e@mail.ru>
2023-04-16 13:00:27 +0300

    startedSignUp

--- app/src/main/java/com/example/session2try1/SignUpActivity.kt
+++ app/src/main/java/com/example/session2try1/SignUpActivity.kt
@@ -2,10 +2,24 @@ package com.example.session2try1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.session2try1.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.back.setOnClickListener {
+            finish()
+        }
+        binding.register.setOnClickListener {
+
+        }
+
     }
+
 }
